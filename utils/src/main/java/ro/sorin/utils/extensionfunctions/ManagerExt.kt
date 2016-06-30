5ea7c1578e9d993d48927797aecee0716eaf799b
greenspand <mihaiirimies@gmail.com>
2016-06-30 15:49:16 +0200

    updated realm plugin

--- utils/src/main/java/ro/sorin/utils/extensionfunctions/ManagerExt.kt
+++ utils/src/main/java/ro/sorin/utils/extensionfunctions/ManagerExt.kt
@@ -2,14 +2,9 @@ package ro.sorin.utils.extensionfunctions
 
 import android.net.wifi.WifiConfiguration
 
-/**
- * Created by sorin on 4/17/16.
- */
-
 
 inline fun wifiConfiguration( func : WifiConfiguration.() -> Unit): WifiConfiguration{
     val config = WifiConfiguration()
     config.func()
     return config.apply(func)
 }
-
